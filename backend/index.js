03162809b03cbb328d4dd51a3636250ec42969b4
RifqiAbbiyu6822 <161233670+RifqiAbbiyu6822@users.noreply.github.com>
2025-04-20 22:37:06 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,4 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5001, () => console.log('Server running on port 5001'));
+app.listen(5000, () => console.log('Server running on port 5001'));
